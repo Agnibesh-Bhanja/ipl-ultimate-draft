2f5050f18ada29e6f30bb382bde11bd3c2447b8b
Agnibesh-Bhanja <agnibeshbhanja@gmail.com>
2026-07-20 18:48:27 +0530

    Fix Scoreboard import case

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+        "https://ipl-ultimate-draft-xavb-nine.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
